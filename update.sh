aa01674d1cabe2616717deb3330eecd9dcb17cbf
Richard Schwab <gitrichardschwab-7a2qxq42kj@central-intelligence.agency>
2021-02-28 21:05:53 +0000

    fix typo in update.sh

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ v="$1"
 
 sed -r -i "" -e 's|OMBI_PACKAGE=.+|OMBI_PACKAGE=https://github.com/Ombi-app/Ombi/releases/download/'"$v"'/linux-x64.tar.gz|' Dockerfile
 git add Dockerfile
-git commit -m update to $v"
+git commit -m "update to $v"
 git tag "$v"
 git push --tags
 git push
